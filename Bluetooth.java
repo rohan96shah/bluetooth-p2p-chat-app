98bc2178330011e446e350ac5678ea00a6198959
Rohan <rohan96shah@gmail.com>
2018-03-04 04:35:56 -0600

    P2P Commit

--- Bluetooth.java
+++ Bluetooth.java
@@ -1,5 +1,7 @@
 package bluetooth;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rohan Shah
@@ -8,14 +10,48 @@ public class bluetooth implements Runnable
 {
     static Thread thread_server;
     static Thread thread_client;
+    static Thread thread_main;
     
     
-    public static void main(String... args)
+    public static void main(String... args) throws InterruptedException
     {
+        
+        
+        Scanner sc = new Scanner(System.in);
+        String inp;
+        
+        thread_main = new Thread(new bluetooth());
         thread_server = new Thread(new SampleSPPServer());
         thread_client = new Thread(new SampleSPPClient());
         
+        boolean checkEnterKeyPress = true;
+        thread_server.start();
+        while(true)
+        {
+            System.out.println("Press 's' to send a message");
+            
+            //thread_server.start();
+            
+            inp = sc.next();
+            
+            if(inp.equals("s"))
+            {
+                //thread_server.join();
+                //thread_server.sleep(10000);
+                break;
+                
+            }
+            else
+            {
+                System.out.println("Incorrect Input. Try again.");
+            }
+            
+        }
         thread_client.start();
+        thread_client.join();
+        
+        main();
+        //}
         
     }
     
