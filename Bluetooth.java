e9221de374e58034d894b47e64ae9254198da469
rohan96shah <rohan96shah@gmail.com>
2018-04-11 16:09:16 -0500

    Update Bluetooth.java

--- Bluetooth.java
+++ Bluetooth.java
@@ -1,174 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bluetooth;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.Vector;
 
-import javax.bluetooth.DeviceClass;
-import javax.bluetooth.DiscoveryAgent;
-import javax.bluetooth.DiscoveryListener;
-import javax.bluetooth.LocalDevice;
-import javax.bluetooth.RemoteDevice;
-import javax.bluetooth.ServiceRecord;
-import javax.bluetooth.UUID;
-import javax.microedition.io.Connector;
-import javax.microedition.io.StreamConnection;
-
+import java.util.Random;
+import java.util.Scanner;
 
 /**
- * A simple SPP client that connects with an SPP server
+ *
+ * @author Rohan Shah
  */
-public class Bluetooth implements DiscoveryListener
+public class Bluetooth implements Runnable
 {
-
-    //object used for waiting
-    private static Object lock = new Object();
-
-    //vector containing the devices discovered
-    private static Vector vecDevices = new Vector();
-
-    private static String connectionURL = null;
-
-    public static void main(String[] args) throws IOException
+    static Thread thread_server;
+    static Thread thread_client;
+    static Thread thread_main;
+    static Random generator;
+    
+    
+    public static void main(String... args) throws InterruptedException
     {
-        Bluetooth client = new Bluetooth();
-        LocalDevice localDevice = LocalDevice.getLocalDevice();
-        System.out.println("Address: " + localDevice.getBluetoothAddress());
-        System.out.println("Name: " + localDevice.getFriendlyName());
-        DiscoveryAgent agent = localDevice.getDiscoveryAgent();
-        System.out.println("Starting device inquiry");
-        agent.startInquiry(DiscoveryAgent.GIAC, client);
-
-        try
-        {
-            synchronized (lock)
-            {
-                lock.wait();
-            }
+        
+        generator = new Random();
+        thread_server = new Thread(new SampleSPPServer());
+        thread_client = new Thread(new SampleSPPClient());        
+        int random = generator.nextInt(2);
+       // int random = 1;
+        
+        
+        System.out.println("Starting main...");
+        System.out.println("Our number is: " + random + ".");
+        
+        if(random == 0) { // start client if the boolean value is 0
+        	Thread.sleep(10000);
+        	thread_client.start();
+        	Thread.sleep(10000);
+        	if(!thread_client.isInterrupted()) {
+        		main();
+        	}
         }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
+        else { // start server if the vollean value is 1
+        	thread_server.run();  
+        	Thread.sleep(40000);
+        	main();
         }
-
-        System.out.println("Device Inquiry Completed. ");
-
-        //print all devices in vecDevices
-        int deviceCount = vecDevices.size();
-
-        if (deviceCount <= 0)
-        {
-            System.out.println("No Devices Found .");
-            System.exit(0);
-        }
-        else
-        {
-            //print bluetooth device addresses and names in the format [ No. address (name) ]
-            System.out.println("Bluetooth Devices: ");
-            for (int i = 0; i < deviceCount; i++)
-            {
-                RemoteDevice remoteDevice = (RemoteDevice) vecDevices.elementAt(i);
-                System.out.println((i + 1) + ". " + remoteDevice.getBluetoothAddress() + " (" + remoteDevice.getFriendlyName(false) + ")");
-            }
-        }
-
-        System.out.print("Choose Device index: ");
-        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
-
-        String chosenIndex = bReader.readLine();
-        int index = Integer.parseInt(chosenIndex.trim());
-
-        //check for spp service
-        RemoteDevice remoteDevice = (RemoteDevice) vecDevices.elementAt(index - 1);
-        UUID[] uuidSet = new UUID[1];
-        uuidSet[0] = new UUID(0x1101);
-
-        System.out.println("\nSearching for service...");
-        agent.searchServices(null, uuidSet, remoteDevice, client);
-
-        try
-        {
-            synchronized (lock)
-            {
-                lock.wait();
-            }
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
-
-        if (connectionURL == null)
-        {
-            System.out.println("Device does not support Simple SPP Service.");
-            System.exit(0);
-        }
-
-        //connect to the server and send a line of text
-        StreamConnection streamConnection = (StreamConnection) Connector.open(connectionURL);
-
-        //send string
-        OutputStream outStream = streamConnection.openOutputStream();
-        PrintWriter pWriter = new PrintWriter(new OutputStreamWriter(outStream));
-        pWriter.write("Test String from SPP Client\r\n");
-        pWriter.flush();
-
-        //read response
-        InputStream inStream = streamConnection.openInputStream();
-        BufferedReader bReader2 = new BufferedReader(new InputStreamReader(inStream));
-        String lineRead = bReader2.readLine();
-        System.out.println(lineRead);
-
-    }//main
-
-    //methods of DiscoveryListener
-    public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod)
-    {
-        //add the device to the vector
-        if (!vecDevices.contains(btDevice))
-        {
-            vecDevices.addElement(btDevice);
-        }
+        
     }
-
-    //implement this method since services are not being discovered
-    public void servicesDiscovered(int transID, ServiceRecord[] servRecord)
+    
+    
+    public void run()
     {
-        if (servRecord != null && servRecord.length > 0)
-        {
-            connectionURL = servRecord[0].getConnectionURL(0, false);
-        }
-        synchronized (lock)
-        {
-            lock.notify();
-        }
+        
     }
-
-    //implement this method since services are not being discovered
-    public void serviceSearchCompleted(int transID, int respCode)
-    {
-        synchronized (lock)
-        {
-            lock.notify();
-        }
-    }
-
-    public void inquiryCompleted(int discType)
-    {
-        synchronized (lock)
-        {
-            lock.notify();
-        }
-
-    }//end method
-
 }
